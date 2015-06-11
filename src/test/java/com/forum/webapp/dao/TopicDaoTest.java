c1c5283412a123d609b56dd187d87b812a4c06fc
CharlesCoque <c.coque@hotmail.fr>
2015-06-11 13:57:19 +0200

    Format class TopicDaoTest

--- src/test/java/com/forum/webapp/dao/TopicDaoTest.java
+++ src/test/java/com/forum/webapp/dao/TopicDaoTest.java
@@ -25,93 +25,91 @@ import com.forum.webapp.repos.ShareRepository;
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false)
 public class TopicDaoTest {
 
-    Long usersId[] = new Long[3];
+	Long usersId[] = new Long[3];
 
-    @Autowired
-    protected ITopicDao topicDao;
+	@Autowired
+	protected ITopicDao topicDao;
 
-    @Autowired
-    protected IUserDao userDao;
+	@Autowired
+	protected IUserDao userDao;
 
-    @Autowired
-    protected ShareRepository shareRepository;
+	@Autowired
+	protected ShareRepository shareRepository;
 
-    @Before
-    public void setup() {
-        UserEntity userEntity = new UserEntity();
-        userEntity.setFirstName("First");
-        userEntity.setName("Creator");
-        userEntity.setEmail("creator@titi.com");
-        userEntity.setPassword("pwd");
-        usersId[0] = userDao.create(userEntity);
+	@Before
+	public void setup() {
+		UserEntity userEntity = new UserEntity();
+		userEntity.setFirstName("First");
+		userEntity.setName("Creator");
+		userEntity.setEmail("creator@titi.com");
+		userEntity.setPassword("pwd");
+		usersId[0] = userDao.create(userEntity);
 
-        userEntity = new UserEntity();
-        userEntity.setFirstName("First");
-        userEntity.setName("AllowedReader");
-        userEntity.setEmail("allowedreader@titi.com");
-        userEntity.setPassword("pwd");
-        usersId[1] = userDao.create(userEntity);
+		userEntity = new UserEntity();
+		userEntity.setFirstName("First");
+		userEntity.setName("AllowedReader");
+		userEntity.setEmail("allowedreader@titi.com");
+		userEntity.setPassword("pwd");
+		usersId[1] = userDao.create(userEntity);
 
-        userEntity = new UserEntity();
-        userEntity.setFirstName("First");
-        userEntity.setName("NotAllowedReader");
-        userEntity.setEmail("notallowedreader@titi.com");
-        userEntity.setPassword("pwd");
-        usersId[2] = userDao.create(userEntity);
-    }
+		userEntity = new UserEntity();
+		userEntity.setFirstName("First");
+		userEntity.setName("NotAllowedReader");
+		userEntity.setEmail("notallowedreader@titi.com");
+		userEntity.setPassword("pwd");
+		usersId[2] = userDao.create(userEntity);
+	}
 
-    @After
-    public void teardown() {
-        for (Long userId : usersId) {
-            userDao.delete(userId);
-        }
-    }
+	@After
+	public void teardown() {
+		for (Long userId : usersId) {
+			userDao.delete(userId);
+		}
+	}
 
-    @Test
-    public void createNewPrivateTopic() {
-        // Check the initial state. A default topic already exists.
-        assertEquals(1, topicDao.list(usersId[0]).size());
-        assertEquals(1, topicDao.list(usersId[1]).size());
+	@Test
+	public void createNewPrivateTopic() {
+		// Check the initial state. A default topic already exists.
+		assertEquals(1, topicDao.list(usersId[0]).size());
+		assertEquals(1, topicDao.list(usersId[1]).size());
 
-        // Create a new private topic.
-        TopicEntity topicEntity = new TopicEntity();
-        topicEntity.setPublicTopic(false);
-        topicEntity.setTitle("Title");
-        topicEntity.setCreatorId(usersId[0]);
-        Long topicId = topicDao.create(topicEntity);
+		// Create a new private topic.
+		TopicEntity topicEntity = new TopicEntity();
+		topicEntity.setPublicTopic(false);
+		topicEntity.setTitle("Title");
+		topicEntity.setCreatorId(usersId[0]);
+		Long topicId = topicDao.create(topicEntity);
 
-        // The creator can see the new topic.
-        List<TopicEntity> topics = topicDao.list(usersId[0]);
-        assertThat(
-                topics,
-                Matchers.<Collection<TopicEntity>>allOf(
-                        Matchers.hasSize(2),
-                        Matchers.<TopicEntity>hasItem(Matchers.hasProperty("id",
-                                Matchers.is(topicId)))));
-        // The other users cannot.
-        topics = topicDao.list(usersId[1]);
-        assertThat(topics, Matchers.<Collection<TopicEntity>>allOf(
-                Matchers.hasSize(1),
-                Matchers.not(Matchers.<TopicEntity>hasItem(Matchers.hasProperty("id",
-                        Matchers.is(topicId))))));
-    }
+		// The creator can see the new topic.
+		List<TopicEntity> topics = topicDao.list(usersId[0]);
+		assertThat(
+		        topics,
+		        Matchers.<Collection<TopicEntity>>allOf(Matchers.hasSize(2),
+		                Matchers.<TopicEntity>hasItem(Matchers.hasProperty("id", Matchers.is(topicId)))));
+		// The other users cannot.
+		topics = topicDao.list(usersId[1]);
+		assertThat(
+		        topics,
+		        Matchers.<Collection<TopicEntity>>allOf(Matchers.hasSize(1),
+		                Matchers.not(Matchers.<TopicEntity>hasItem(Matchers.hasProperty("id", Matchers.is(topicId))))));
+	}
 
-    @Test
-    public void createNewPublicTopic() {
-        // TODO Create a new public topic and check if all the users can have it
-        // in their list.
-    }
+	@Test
+	public void createNewPublicTopic() {
+		// TODO Create a new public topic and check if all the users can have it
+		// in their list.
+	}
 
-    @Test
-    public void sharePrivateTopic() {
-        // TODO Create a new private topic, share it with the user[1] and check
-        // if it can have it
-        // in its list, but not the user[2].
+	@Test
+	public void sharePrivateTopic() {
+		// TODO Create a new private topic, share it with the user[1] and check
+		// if it can have it
+		// in its list, but not the user[2].
 
-        // Create a share on the topic.
-        /*
-         * ShareEntity share = new ShareEntity(); share.setTopicId(topicId);
-         * share.setReaderId(usersId[1]); shareRepository.save(share);
-         */
-    }
+		// Create a share on the topic.
+		/*
+		 * ShareEntity share = new ShareEntity(); share.setTopicId(topicId);
+		 * share.setReaderId(usersId[1]); shareRepository.save(share);
+		 */
+	}
 }
